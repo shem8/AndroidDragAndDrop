7e1931bde5cbcfabad8b2784a9bc22b9761a7037
Shem Magnezi <shem@myroll.com>
2015-09-04 15:12:03 +0300

    implement the drag and drop

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AndroidDragAndDrop
 An example project for using built in drag and drop functionality in Android between views
+
+More about it in [my blog](https://shem8.wordpress.com/2015/09/04/android-drag-and-drop/)
+
+![ex_img](https://shem8.files.wordpress.com/2015/09/draganddrop.gif) 
--- app/src/main/java/shem/androiddraganddrop/MainActivityFragment.java
+++ app/src/main/java/shem/androiddraganddrop/MainActivityFragment.java
@@ -1,22 +1,81 @@
 package shem.androiddraganddrop;
 
+import android.content.ClipData;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
-    }
+    private View[] views;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+        views = new View[rootView.getChildCount()];
+        for (int i = 0; i < rootView.getChildCount(); i++) {
+            final TextView view = (TextView) rootView.getChildAt(i);
+            initView(view);
+            views[i] = view;
+        }
+        return rootView;
     }
+
+    private void initView(final TextView view) {
+        view.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                switch(event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        v.setBackgroundColor(Color.LTGRAY);
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        v.setBackgroundColor(Color.TRANSPARENT);
+                        return true;
+
+                    case DragEvent.ACTION_DROP:
+                        v.setBackgroundColor(Color.TRANSPARENT);
+                        int dragVal = Integer.parseInt(event.getClipData().getItemAt(0).getText().toString());
+                        int viewVal = Integer.parseInt(((TextView) v).getText().toString());
+                        ((TextView) v).setText("" + (dragVal + viewVal));
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        return true;
+
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData data = ClipData.newPlainText("value", view.getText());
+                view.startDrag(data, new View.DragShadowBuilder(v), null, 0);
+                return true;
+            }
+        });
+    }
+
 }
